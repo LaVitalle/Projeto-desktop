548a03a7d8ee78168c499f685e748c4fc0391eb1
Vitor <vitor.fernandes@alunos.bpkedu.com.br>
2024-05-27 15:26:40 -0300

    Subindo

--- Códigos/main.c
+++ Códigos/main.c
@@ -15,6 +15,136 @@ int menuFunc()
     return menuOps;
 }
 
+void entrada()
+{
+
+    int opc=3;
+
+    while(opc==3)
+    {
+        printf("\nCLIENTE NÃO CADASTRADO _1_\nCLIENTE CADASTRADO _2_\nVOLTAR _0_\n");
+        scanf("%i", &opc);
+        if(opc!=0 && opc!=1 && opc!=2){printf("\nERRO!!\n"); opc=3;}
+    }
+
+    return;
+}
+
+void saida()
+{
+
+    int opc=3;
+
+    while(opc==3)
+    {
+        printf("\nCLIENTE NÃO CADASTRADO _1_\nCLIENTE CADASTRADO _2_\nVOLTAR _0_\n");
+        scanf("%i", &opc);
+        if(opc!=0 && opc!=1 && opc!=2){printf("\nERRO!!\n"); opc=3;}
+    }
+
+    return;
+}
+
+void gerenciamento()
+{
+
+    int check=1;
+
+    while (check==1)
+    {
+        int opc=5;
+
+        while(opc==5)
+        {
+            printf("\nCLIENTES _1_\nVAGAS _2_\nPLANOS _3_\nRELATÓRIOS _4_\nVOLTAR _0_\n");
+            scanf("%i", &opc);
+            if(opc!=0 && opc!=1 && opc!=2 && opc!=3 && opc!=4){printf("\nERRO!!\n"); opc=5;}
+        }
+
+        if(opc==1)
+        {
+            menuClientes();
+        }
+        else if(opc==2)
+        {
+            menuVagas();
+        }
+        else if(opc==3)
+        {
+            menuPlanos();
+        }
+        else if(opc==4)
+        {
+            menuRelatorios();
+        }
+        else if (opc==0)
+        {
+            check=0;
+        }
+    }
+    return;
+}
+
+void menuClientes()
+{
+
+    int opc=5;
+
+    while(opc==5)
+    {
+        printf("\nCADASTRAR CLIENTE _1_\nEDITAR PLANO _2_\nREMOVER CADASTRO _3_\nVIZUALIZAR CLIENTES _4_\nVOLTAR _0_\n");
+        scanf("%i", &opc);
+        if(opc!=0 && opc!=1 && opc!=2 && opc!=3 && opc!=4){printf("\nERRO!!\n"); opc=5;}
+    }
+
+    return;
+}
+
+void menuVagas()
+{
+
+    int opc=3;
+
+    while(opc==3)
+    {
+        printf("\nATUALIZAR VAGAS _1_\nVIZUALIZAR VAGAS _2_\nVOLTAR _0_\n");
+        scanf("%i", &opc);
+        if(opc!=0 && opc!=1 && opc!=2){printf("\nERRO!!\n"); opc=3;}
+    }
+
+    return;
+}
+
+void menuPlanos()
+{
+
+    int opc=5;
+
+    while(opc==5)
+    {
+        printf("\nVIZUALIZAR PLANOS _1_\nEDITAR PLANOS _2_\nCADASTRAR PLANOS _3_\nREMOVER PLANOS _4_\nVOLTAR _0_\n");
+        scanf("%i", &opc);
+        if(opc!=0 && opc!=1 && opc!=2 && opc!=3 && opc!=4){printf("\nERRO!!\n"); opc=5;}
+    }
+
+    return;
+}
+
+void menuRelatorios()
+{
+    
+    int opc=5;
+
+    while(opc==5)
+    {
+        printf("\nRELATÓRIO DE CLIENTES CADASTRADOS _1_\nRELATÓRIO DE CLIENTES NÃO CADASTRADOS _2_\nRELATÓRIO DE VAGAS _3_\nRELATÓRIO DIÁRIO _4_\nVOLTAR _0_\n");
+        scanf("%i", &opc);
+        if(opc!=0 && opc!=1 && opc!=2 && opc!=3 && opc!=4){printf("\nERRO!!\n"); opc=5;}
+    }
+
+    return;
+}
+
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
@@ -36,7 +166,7 @@ int main()
         }
         else if(menuOps==3)
         {
-            gerenciamento();    //Deve conter um menu com as opções Cadastrar, Vagas, Planos e Relatórios.
+            gerenciamento();
         }
         else if(menuOps==0)
         {
